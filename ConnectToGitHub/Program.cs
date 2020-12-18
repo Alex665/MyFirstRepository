e84a986f768f7e0820f3e5fd6dfe2c3d8285e224
Alex665 <56033215+Alex665@users.noreply.github.com>
2020-12-18 16:50:30 +0300

    Added Console.WriteLine

--- ConnectToGitHub/Program.cs
+++ ConnectToGitHub/Program.cs
@@ -4,6 +4,7 @@ namespace ConnectToGitHub {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Первое знакомоство с GitHub!");
+            Console.WriteLine("Проверка изменения на сервере!");
         }
     }
 }
