a66587d1d030cea9084178d25810f1d13a6c917d
Alex665 <56033215+Alex665@users.noreply.github.com>
2020-12-18 16:20:34 +0300

    Добавьте файлы проекта.

---
+++ ConnectToGitHub/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ConnectToGitHub {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Первое знакомоство с GitHub!");
+        }
+    }
+}
